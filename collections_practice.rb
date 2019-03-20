e7bfd7a812deb8e601573b8c529fd7b45c264fee
Francisco Costa <francisco@fcosta.pt>
2019-03-20 16:07:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,16 @@
 require 'pry'
 
 def begins_with_r(array)
-  array.each do |word|
-    if word[0] != "r"
-      return false
-    end
-  end
+
+  array.each {|word| if word[0] != "r" then return false end}
   true
+
+end
+
+def contain_a(array)
+
+  words_with_a = []
+  array.each {|word| if word.split("").include?("a") then words_with_a << word end}
+  words_with_a
+
 end

1ff73eebf7e9b7f7294ebbe6d03a5e138752982b
Adnan Azim <saazim1123@gmail.com>
2019-03-23 18:20:38 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,6 +19,49 @@ def first_wa(array) #Return the first element that begins with the letters 'wa'
   end
 end
 
-def non_strings(array)
-  
+def remove_non_strings(array) #remove anything that's not a string from an array
+  array.reject do |word|
+    word.class != String
+  end
+end
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|word| counts[word]+= 1}
+    counts.collect do |key, value|
+      key[:count] = value
+    end
+  counts.keys
+end
+
+def merge_data(keys, data)
+  keys.each do |name_hash|
+    data.each do |name|
+      name_hash.merge!(name[name_hash[:first_name]])
+    end
+  end
+end
+
+def find_cool(array)
+  array.select do |hash|
+    if hash.has_value?("cool")
+      hash
+    end
+  end
+end
+
+def organize_schools(array) #organizes the schools by location
+  hash = {}
+  array.each do |schools, locations|
+    locations.each do |school, location|
+      if hash[location] == nil 
+        hash[location] = [schools]
+        
+      else
+        hash[location] << schools
+        
+      end
+    end
+  end
+  hash
 end

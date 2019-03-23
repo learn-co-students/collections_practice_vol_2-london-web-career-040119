062ebec543fa562d256048ef858b64db8b3d5de3
Adnan Azim <saazim1123@gmail.com>
2019-03-23 09:51:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,24 @@
 # your code goes here
+
+def begins_with_r(array) #Return true if every element of the tools array starts with an "r" and false otherwise.
+  array.all? do |word|
+    word.downcase.chr == "r"
+     
+  end
+end
+
+def contain_a(array) # return all elements that contain the letter 'a'
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array) #Return the first element that begins with the letters 'wa'
+  array.find do |word|
+    word.to_s.start_with?("wa")
+  end
+end
+
+def non_strings(array)
+  
+end
